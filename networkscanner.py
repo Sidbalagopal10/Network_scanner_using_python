a87f3e5a97fd77dda39e86e1de9f89acc5d228b9
Siddarth Balagopal <sidbalagopal2002@gmail.com>
2025-05-15 18:09:23 -0500

    networkscanner.py

--- networkscanner.py
+++ networkscanner.py
@@ -1,23 +1,43 @@
-from scapy.all import ARP, Ether, srp
+from scapy.all import ARP, Ether, srp, conf
+import sys
 
-target_ip = "your ip addresss"
-# IP Address for the destination
-# create ARP packet
-arp = ARP(pdst=target_ip)
-ether = Ether(dst="ff:ff:ff:ff:ff:ff")
-packet = ether/arp
+def scan_network(target_ip, interface=None):
+    try:
+        # Set interface if provided
+        if interface:
+            conf.iface = interface
 
-result = srp(packet, timeout=3, verbose=0)[0]
+        # Create ARP packet
+        arp = ARP(pdst=target_ip)
+        ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+        packet = ether / arp
 
-# a list of clients, we will fill this in the upcoming loop
-clients = []
+        # Send the packet and receive responses
+        result = srp(packet, timeout=3, verbose=0)[0]
 
-for sent, received in result:
-    # for each response, append ip and mac address to `clients` list
-    clients.append({'ip': received.psrc, 'mac': received.hwsrc})
+        clients = []
 
-# print clients
-print("Available devices in the network:")
-print("IP" + " "*18+"MAC")
-for client in clients:
-    print("{:16}    {}".format(client['ip'], client['mac']))
+        for sent, received in result:
+            clients.append({'ip': received.psrc, 'mac': received.hwsrc})
+
+        # Print the result
+        print("Available devices in the network:")
+        print("IP" + " " * 18 + "MAC")
+        for client in clients:
+            print("{:16}    {}".format(client['ip'], client['mac']))
+
+        if not clients:
+            print("\nNo devices found. Make sure target IP/subnet is reachable.")
+
+    except PermissionError:
+        print("Error: Run this script as administrator/root (use sudo).")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+# Change this to your subnet (e.g., "192.168.29.0/24")
+target_ip_range = "enter ip range"
+
+# Optional: Set this if you have multiple interfaces (e.g., "eth0", "wlan0")
+network_interface = "enter type of network interface"
+
+scan_network(target_ip_range, network_interface)
